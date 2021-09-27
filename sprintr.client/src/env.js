6179524495ebd720a80e009acb31daf807868318
darius <dariusdeah21@gmail.com>
2021-09-27 14:04:49 -0600

    setup .1

--- sprintr.client/src/env.js
+++ sprintr.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'demodays1.us.auth0.com'
+export const clientId = 'IZeGI4ViVhH3sgXw0pSEpokDdTt8FI8X'
+export const audience = 'https://student-auth.com'
