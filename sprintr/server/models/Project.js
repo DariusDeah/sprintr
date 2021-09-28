2b5d5eb9d9ab42d5de9bc1bbabe426fbd9689ad3
darius <dariusdeah21@gmail.com>
2021-09-28 12:38:02 -0600

    code clean up

--- sprintr/server/models/Project.js
+++ sprintr/server/models/Project.js
@@ -6,7 +6,9 @@ export const ProjectSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   creatorId: { type: Schema.Types.ObjectId, ref: 'Profile', required: true }
-})
+},
+{ timestamps: true, toJSON: { virtuals: true } }
+)
 
 ProjectSchema.virtual('creator name picture', {
   localField: 'creatorId',
