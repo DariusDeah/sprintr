90945f3f9a1c03529afda0cb424d943958d1f198
darius <dariusdeah21@gmail.com>
2021-09-27 19:41:21 -0600

    Project get methods completed

--- sprintr/server/controllers/ProjectController.js
+++ sprintr/server/controllers/ProjectController.js
@@ -1,20 +1,31 @@
-import { projectService } from '../services/projectservice'
+import { projectService } from '../services/ProjectService'
 import BaseController from '../utils/BaseController'
 import { logger } from '../utils/Logger'
 
 export class ProjectController extends BaseController {
-  // constructor() {
-  //   super('api/projects')
-  //   this.router
-  //     .get('', this.getProjects)
-  // }
+  constructor() {
+    super('api/projects')
+    this.router
+      .get('', this.getProjects)
+      .get('/:id', this.getProjectsById)
+  }
 
-  // async getProjects(req, res, next) {
-  //   try {
-  //     const res = await projectService.getProjects()
-  //     res.send()
-  //   } catch (error) {
-  //     next(error)
-  //   }
-  // }
+  // GETS REQUESTS
+  async getProjects(req, res, next) {
+    try {
+      const projects = await projectService.getProjects()
+      res.send(projects)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getProjectsById(req, res, next) {
+    try {
+      const project = await projectService.getProjectsById(req.params.projectId)
+      res.send(project)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
--- sprintr/server/services/projectservice.js
+++ sprintr/server/services/projectservice.js
@@ -1,9 +1,19 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 
 class ProjectService {
   async getProjects() {
-    const res = await dbContext.Projects.find()
-    return res
+    const projects = await dbContext.Projects.find()
+    return projects
+  }
+
+  // finds a project by the id (projectId) if that project doesnt exesit a bad request is thrown
+  async getProjectsById(projectId) {
+    const project = await dbContext.Projects.findById(projectId).populate('creator')
+    if (!project) {
+      throw new BadRequest()
+    }
+    return project
   }
 }
 export const projectService = new ProjectService()
