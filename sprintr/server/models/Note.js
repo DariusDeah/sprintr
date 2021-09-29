5fa88bdcf45ebb3e7e4cc68a6f1758fcf1005d84
Quinnton <quinnclimbs@gmail.com>
2021-09-28 21:40:00 -0600

    BacklogItems Updated

--- sprintr/server/models/Note.js
+++ sprintr/server/models/Note.js
@@ -6,5 +6,13 @@ export const NoteSchema = new Schema({
   body: { type: String, required: true },
   taskId: { type: Schema.Types.ObjectId },
   projectId: { type: Schema.Types.ObjectId, ref: 'Projects' },
-  creatorId: { types: Schema.Types.ObjectId }
+  creatorId: { types: Schema.Types.ObjectId, ref: 'Profile' }
+},
+{ timestamps: true, toJSON: { virtuals: true } })
+
+NoteSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Profile'
 })
