1af7c11b62ac4ba34498db02a42e334cf4534c7f
darius <dariusdeah21@gmail.com>
2021-09-30 22:10:46 -0600

    working

--- sprintr.client/src/components/Child-nav.vue
+++ sprintr.client/src/components/Child-nav.vue
@@ -1,9 +1,12 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-primary px-3 ">
-    <div class="col-lg-6">
+    <div class="col-lg-6 d-flex">
       <h1 class="text-light fs-1">
         {{ project.name }}
       </h1>
+      <button class="btn btn-danger" @click="removeProject()">
+        delete project
+      </button>
     </div>
     <div class="col-lg-6">
       <ul>
@@ -23,15 +26,32 @@
 <script>
 import { computed, ref } from '@vue/runtime-core'
 import { AppState } from '../AppState'
+import Pop from '../utils/Pop'
+import { projectService } from '../services/ProjectService'
+import { ProjectModel } from '../Models/Project'
 export default {
-  setup() {
+  props: {
+    projects: { type: ProjectModel, required: true }
+  },
+  setup(props) {
     const editable = ref({})
     return {
       editable,
       project: computed(() => AppState.activeProject),
       toggleBacklogForm() {
         document.getElementById('BacklogForm').classList.toggle('visually-hidden')
+      },
+      async removeProject() {
+        try {
+          if (await Pop.confirm()) {
+            await projectService.removeProject(props.projects.id)
+            Pop.toast('project deleted')
+          }
+        } catch (error) {
+          Pop.toast(error, 'error')
+        }
       }
+
     }
   }
 }
